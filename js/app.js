f8a043a96d33c9e06d860fcbe38b004296c9d1b6
sylvanou <sylvanoumbac24@gmail.com>
2018-01-06 16:15:26 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const dog_api = 'https://dog.ceo/api/breeds/list/all'
+const viewDog = document.querySelector('#viewDog')
+const selectBreed = document.querySelector('#selectBreed')
+let breedImage = document.querySelector('#breedImage')
+
+function getDogBreeds(){
+    $.ajax({
+        type: "GET",
+        url: dog_api,
+        dataType: "json",
+        success: function(data) {
+            // console.log(data)
+            for(let i in data.message){
+                selectBreed.innerHTML += `
+                <option value="${i}">${i}</option>
+                `
+            }
+        },
+        error: function(error){
+            console.log(error)
+        }
+})
+}
+
+getDogBreeds()
+
+viewDog.addEventListener('click', function(e){
+    let breedName = selectBreed.value
+    $.ajax({
+        type: "GET",
+        url: `https://dog.ceo/api/breed/${breedName}/images/random`,
+        dataType: "json",
+        success: function(data) {
+            breedImage.setAttribute('src', data.message)
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+})
+
+
